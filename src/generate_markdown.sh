6e4a7966af0458e6ffd0a0a0be168e0e6b6dfada
timovdk <5330531+timovdk@users.noreply.github.com>
2025-09-22 17:39:14 +0200

    update marker slurm script

--- src/generate_markdown.sh
+++ src/generate_markdown.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_h100
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 
 module load 2025 Python/3.13.1-GCCcore-14.2.0 CUDA/12.8.0
 
@@ -11,6 +11,9 @@ export 'PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True'
 
 source $HOME/venvs/grolts_embed/bin/activate
 
-marker ./data/*.pdf --output_dir ./processed_pdfs --workers 10
+marker ./data/ptsd/*.pdf --output_dir ./processed_pdfs/ptsd --workers 10
+marker ./data/achievement/*.pdf --output_dir ./processed_pdfs/achievement --workers 10
+marker ./data/delinquency/*.pdf --output_dir ./processed_pdfs/delinquency --workers 10
+marker ./data/wellbeing/*.pdf --output_dir ./processed_pdfs/wellbeing --workers 10
 
 exit 0
